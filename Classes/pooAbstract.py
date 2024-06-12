724bed6803971a5960ef860d6d6024e86dfc6cf1
unicamp_nascimento <unicamp_nascimento@hcpa.edu.br>
2024-06-12 11:27:50 -0300

    POO e POO abastract

--- Classes/pooAbstract.py
+++ Classes/pooAbstract.py
@@ -4,6 +4,19 @@ from abc import ABC, abstractmethod
 Usar classes abstratas e métodos abstratos com o módulo abc ajuda a garantir 
 que certas funcionalidades sejam implementadas em todas as subclasses, proporcionando 
 uma maneira estruturada de definir contratos para as classes derivadas.
+
+Se fizesse apenas import abc teria que usar abc.Classe:
+import abc
+class Animal(abc.ABC):
+    @abc.abstractmethod
+    def make_sound(self):
+        pass
+class Dog(Animal):
+    def make_sound(self):
+        print("Woof!")
+class Cat(Animal):
+    def make_sound(self):
+        print("Meow!")
 """
 class Animal(ABC): #Se torna abstrata ao herdar de ABC
     def __init__(self, nome, idade):
